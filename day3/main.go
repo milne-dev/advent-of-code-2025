9a837e678e5381070b0ac8d6915ad140c6a48d19
Derek Milne <derek.m.m.77@gmail.com>
2025-12-02 23:15:00 -0800

    complete p1 of day 3

--- day3/main.go
+++ day3/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"aoc2025/utils"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
@@ -13,20 +12,36 @@ func main() {
 		panic(err)
 	}
 
-	fields := strings.Fields(s)
-	var banks []int
-	for _, field := range fields {
-		i, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		banks = append(banks, i)
-	}
+	banks := strings.Fields(s)
 
 	fmt.Println(PartOne(banks))
 }
 
-func PartOne(banks []int) int {
-	fmt.Println(banks)
-	return 0
+func PartOne(banks []string) int {
+	var ans int
+	for _, bank := range banks {
+		ans += MaxNum(bank)
+	}
+	return ans
+}
+
+func MaxNum(bank string) int {
+	prefix := make([]int, len(bank))
+	suffix := make([]int, len(bank))
+
+	prefix[0] = int(bank[0] - '0')
+	for i := 1; i < len(bank); i++ {
+		prefix[i] = max(prefix[i-1], int(bank[i]-'0'))
+	}
+
+	suffix[len(suffix)-1] = int(bank[len(bank)-1] - '0')
+	for i := len(bank) - 2; i >= 0; i-- {
+		suffix[i] = max(suffix[i+1], int(bank[i]-'0'))
+	}
+
+	var ans int
+	for i := 0; i < len(bank)-1; i++ {
+		ans = max(ans, prefix[i]*10+suffix[i+1])
+	}
+	return ans
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestPartOne(t *testing.T) {
-	input := []int{987654321111111, 811111111111119, 234234234234278, 818181911112111}
+	input := []string{"987654321111111", "811111111111119", "234234234234278", "818181911112111"}
 	want := 357
 	got := PartOne(input)
 	if want != got {
