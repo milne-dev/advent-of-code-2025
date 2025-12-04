613f46a3097f28083f267109393d1d0fbd426fe8
Derek Milne <derek.m.m.77@gmail.com>
2025-12-03 21:14:42 -0800

    done part 1

--- day4/main.go
+++ day4/main.go
@@ -17,5 +17,31 @@ func main() {
 }
 
 func PartOne(grid []string) int {
-	return 0
+	var total int
+	for x := range grid {
+		for y := range grid[x] {
+			if grid[x][y] == '@' && canAccess(grid, x, y) {
+				total++
+			}
+		}
+	}
+	return total
+}
+
+var dirs = [][]int{{1, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+
+// can access if fewer than 4 in 8 edges
+func canAccess(grid []string, x, y int) bool {
+	var total int
+	for _, dir := range dirs {
+		i, j := dir[0], dir[1]
+		a, b := x+i, y+j
+		if a < 0 || a == len(grid) || b < 0 || b == len(grid[a]) {
+			continue
+		}
+		if grid[a][b] == '@' {
+			total++
+		}
+	}
+	return total < 4
 }
