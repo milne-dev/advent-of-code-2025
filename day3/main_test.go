44142e28b36a528d9bb2598b3fa051a7a3376d97
Derek Milne <derek.m.m.77@gmail.com>
2025-12-03 17:35:58 -0800

    add benchmark for day3 part2

--- day3/main_test.go
+++ day3/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"aoc2025/utils"
+	"strings"
+	"testing"
+)
 
 func TestPartOne(t *testing.T) {
 	input := []string{"987654321111111", "811111111111119", "234234234234278", "818181911112111"}
@@ -19,5 +23,19 @@ func TestPartTwo(t *testing.T) {
 	if want != got {
 		t.Errorf(`PartTwo(...) want %v, got %v`,
 			want, got)
+	}
+}
+
+// BenchmarkPartTwo-8   	     904	   1318104 ns/op
+func BenchmarkPartTwo(b *testing.B) {
+	s, err := utils.ReadFileText("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	banks := strings.Fields(s)
+
+	for b.Loop() {
+		PartTwo(banks)
 	}
 }
