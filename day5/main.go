822f70a82c87e83d5d20e1e0fb9ab3beb347a41f
Derek Milne <derek.m.m.77@gmail.com>
2025-12-04 17:34:58 -0800

    day5 stub

---
+++ day5/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"aoc2025/utils"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fields := strings.Fields(utils.ReadInput())
+	fmt.Println(PartOne(fields))
+}
+
+func PartOne(fields []string) int {
+	return 0
+}
