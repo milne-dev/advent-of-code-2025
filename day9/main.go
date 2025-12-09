d3cffc4e059b968cb33f2455b69cf61be21db4d1
Derek Milne <derek.m.m.77@gmail.com>
2025-12-08 21:10:52 -0800

    p1 comp

--- day9/main.go
+++ day9/main.go
@@ -11,8 +11,30 @@ func main() {
 	fmt.Println(PartOne(lines))
 }
 
+type point struct {
+	x, y int
+}
+
 func PartOne(lines []string) int {
-	return 0
+	var points []point
+	for _, line := range lines {
+		var p point
+		fmt.Sscanf(line, "%d,%d", &p.x, &p.y)
+		points = append(points, p)
+	}
+
+	var ans int
+	for i, p := range points {
+		for j := i + 1; j < len(points); j++ {
+			q := points[j]
+			// l * w
+			w := max(p.y, q.y) - min(p.y, q.y) + 1
+			l := max(p.x, q.x) - min(p.x, q.x) + 1
+			area := l * w
+			ans = max(ans, area)
+		}
+	}
+	return ans
 }
 
 func PartTwo(lines []string) int {
