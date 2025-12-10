15ca85d6eea1330bd0eccf04a3864f7f20c8df9d
Derek Milne <derek.m.m.77@gmail.com>
2025-12-09 21:17:22 -0800

    target and buttons parsing

--- day10/main.go
+++ day10/main.go
@@ -11,6 +11,66 @@ func main() {
 	fmt.Println(PartOne(lines))
 }
 
+const (
+	ModeTarget = iota
+	ModeButtons
+	ModeVoltage
+)
+
 func PartOne(lines []string) int {
-	return 0
+	var ans int
+
+	for _, line := range lines {
+		var target []bool
+		var buttons [][]int
+
+		mode := ModeTarget
+		var buttonGroup []int
+		var buttonNum int
+
+		for i := 1; i < len(line); i++ {
+			c := line[i]
+
+			if c == ' ' || c == '(' {
+				continue
+			}
+
+			switch mode {
+			case ModeTarget:
+				if c == ']' {
+					mode = ModeButtons
+					continue
+				}
+
+				if c == '.' {
+					target = append(target, false)
+				} else {
+					target = append(target, true)
+				}
+			case ModeButtons:
+				// don't handle voltage for this problem
+				if c == '{' {
+					break
+				}
+
+				switch c {
+				case ',':
+					buttonGroup = append(buttonGroup, buttonNum)
+					buttonNum = 0
+				case ')':
+					buttonGroup = append(buttonGroup, buttonNum)
+					buttonNum = 0
+					buttons = append(buttons, buttonGroup)
+					buttonGroup = []int{}
+				default:
+					buttonNum = buttonNum*10 + int(c-'0')
+				}
+			}
+		}
+
+		fmt.Println(target)
+		fmt.Println(buttons)
+	}
+
+	return ans
 }
