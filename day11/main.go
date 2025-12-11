bea5081dc653cc3993365b447faefb9bac47414a
Derek Milne <derek.m.m.77@gmail.com>
2025-12-10 21:10:50 -0800

    complete p1

--- day11/main.go
+++ day11/main.go
@@ -15,7 +15,18 @@ func main() {
 func PartOne(lines []string) int {
 	adj := make(map[string][]string)
 	for _, line := range lines {
-		adj[line[0:4]] = strings.Fields(line[5:])
+		adj[line[0:3]] = strings.Fields(line[5:])
 	}
-	return 0
+	return search(adj, adj["you"])
+}
+
+func search(adj map[string][]string, edges []string) int {
+	var ans int
+	for _, edge := range edges {
+		if edge == "out" {
+			return 1
+		}
+		ans += search(adj, adj[edge])
+	}
+	return ans
 }
