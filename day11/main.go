848553942fd1369540a99803f4d133f071622b75
Derek Milne <derek.m.m.77@gmail.com>
2025-12-10 21:05:21 -0800

    setup adj list

--- day11/main.go
+++ day11/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2025/utils"
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -12,5 +13,9 @@ func main() {
 }
 
 func PartOne(lines []string) int {
+	adj := make(map[string][]string)
+	for _, line := range lines {
+		adj[line[0:4]] = strings.Fields(line[5:])
+	}
 	return 0
 }
