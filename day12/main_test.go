3dbd88bb879e0562c86b3029c0c6a536065219e7
Derek Milne <derek.m.m.77@gmail.com>
2025-12-11 21:08:09 -0800

    setup test 1

--- day12/main_test.go
+++ day12/main_test.go
@@ -6,8 +6,40 @@ import (
 )
 
 func TestPartOne(t *testing.T) {
-	input := ``
-	want := 1
+	input := `0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2`
+	want := 2
 	got := PartOne(utils.StringLines(input))
 	if want != got {
 		t.Errorf(`PartOne(...) want %v, got %v`,
